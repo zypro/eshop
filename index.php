9ee56395f45bdf359b0697035c47795196aacf8c
Valijon <parliament888@gmail.com>
2015-07-07 20:53:27 +0500

    Постраничная навигация

--- index.php
+++ index.php
@@ -63,7 +63,6 @@
 	include 'include/block-news.php';
 ?>
 	</div>
-
 		<div id="block-content">
 			<div id="block-sorting">
 				<p id="nav-breadcrumbs"><a href="index.php">Главная страница</a> \ <span>Все товары</span></p>
@@ -84,9 +83,25 @@
 				</ul>
 			</div>
 				<ul id="block-tovar-grid">
-			<?php 
-				$result = mysql_query("SELECT * FROM table_products WHERE visible = '1' ORDER BY $sorting",$link);
-				if (mysql_num_rows($result) > 0) 
+			<?php
+			// Постраничная навигация
+	$num = 5;
+	$page = (int)$_GET['page'];
+	$count = mysql_query("SELECT COUNT(*) FROM table_products WHERE visible = '1'",$link);
+	$temp = mysql_fetch_array($count);
+	If ($temp[0] > 0)
+	{
+	$tempcount = $temp[0];
+	$total = (($tempcount - 1) / $num) + 1;
+	$total =  intval($total);
+	$page = intval($page);
+	if(empty($page) or $page < 0) $page = 1;
+	if($page > $total) $page = $total;
+	$start = $page * $num - $num;
+	$qury_start_num = " LIMIT $start, $num";
+	}
+				$result = mysql_query("SELECT * FROM table_products WHERE visible = '1' ORDER BY $sorting $qury_start_num",$link);
+				if (mysql_num_rows($result) > 0)
 				{
 					$row = mysql_fetch_array($result);
 					do
@@ -94,20 +109,20 @@
 					if ($row["image"] != "" && file_exists("./upload_images/".$row["image"]))
 						{
 						$img_path = './upload_images/'.$row["image"];
-						$max_width = 200; 
-						$max_height = 200; 
+						$max_width = 200;
+						$max_height = 200;
 						 list($width, $height) = getimagesize($img_path); 
-						$ratioh = $max_height/$height; 
-						$ratiow = $max_width/$width; 
-						$ratio = min($ratioh, $ratiow); 
-						$width = intval($ratio*$width); 
-						$height = intval($ratio*$height);    
+						$ratioh = $max_height/$height;
+						$ratiow = $max_width/$width;
+						$ratio = min($ratioh, $ratiow);
+						$width = intval($ratio*$width);
+						$height = intval($ratio*$height);
 						}else
 						{
 						$img_path = "/images/no-image.png";
 						$width = 110;
 						$height = 200;
-						} 
+						}
 						echo '
 								<li>
 									<div class="block-images-grid">
@@ -129,8 +144,8 @@
 			 ?>
 			 	</ul>
 			 	<ul id="block-tovar-list">
-			<?php 
-				$result = mysql_query("SELECT * FROM table_products WHERE visible = '1' ORDER BY $sorting",$link);
+			<?php
+				$result = mysql_query("SELECT * FROM table_products WHERE visible = '1' ORDER BY $sorting $qury_start_num",$link);
 				if (mysql_num_rows($result) > 0)
 				{
 					$row = mysql_fetch_array($result);
@@ -139,20 +154,20 @@
 					if ($row["image"] != "" && file_exists("./upload_images/".$row["image"]))
 						{
 						$img_path = './upload_images/'.$row["image"];
-						$max_width = 150; 
-						$max_height = 150; 
-						 list($width, $height) = getimagesize($img_path); 
-						$ratioh = $max_height/$height; 
-						$ratiow = $max_width/$width; 
-						$ratio = min($ratioh, $ratiow); 
-						$width = intval($ratio*$width); 
-						$height = intval($ratio*$height);    
+						$max_width = 150;
+						$max_height = 150;
+						 list($width, $height) = getimagesize($img_path);
+						$ratioh = $max_height/$height;
+						$ratiow = $max_width/$width;
+						$ratio = min($ratioh, $ratiow);
+						$width = intval($ratio*$width);
+						$height = intval($ratio*$height);
 						}else
 						{
 						$img_path = "/images/noimages80x70.png";
 						$width = 80;
 						$height = 70;
-						} 
+						}
 						echo '
 								<li>
 									<div class="block-images-list">
@@ -171,13 +186,50 @@
 					}
 						while($row = mysql_fetch_array($result));
 				}
-			 ?>
-			 	</ul>
+						echo '</ul>';
+if ($page != 1){ $pstr_prev = '<li><a class="pstr-prev" href="index.php?page='.($page - 1).'">&lt;</a></li>';}
+if ($page != $total) $pstr_next = '<li><a class="pstr-next" href="index.php?page='.($page + 1).'">&gt;</a></li>';
+
+
+// Формируем ссылки со страницами
+if($page - 5 > 0) $page5left = '<li><a href="index.php?page='.($page - 5).'">'.($page - 5).'</a></li>';
+if($page - 4 > 0) $page4left = '<li><a href="index.php?page='.($page - 4).'">'.($page - 4).'</a></li>';
+if($page - 3 > 0) $page3left = '<li><a href="index.php?page='.($page - 3).'">'.($page - 3).'</a></li>';
+if($page - 2 > 0) $page2left = '<li><a href="index.php?page='.($page - 2).'">'.($page - 2).'</a></li>';
+if($page - 1 > 0) $page1left = '<li><a href="index.php?page='.($page - 1).'">'.($page - 1).'</a></li>';
+
+if($page + 5 <= $total) $page5right = '<li><a href="index.php?page='.($page + 5).'">'.($page + 5).'</a></li>';
+if($page + 4 <= $total) $page4right = '<li><a href="index.php?page='.($page + 4).'">'.($page + 4).'</a></li>';
+if($page + 3 <= $total) $page3right = '<li><a href="index.php?page='.($page + 3).'">'.($page + 3).'</a></li>';
+if($page + 2 <= $total) $page2right = '<li><a href="index.php?page='.($page + 2).'">'.($page + 2).'</a></li>';
+if($page + 1 <= $total) $page1right = '<li><a href="index.php?page='.($page + 1).'">'.($page + 1).'</a></li>';
+
+if ($page+5 < $total)
+{
+$strtotal = '<li><p class="nav-point">...</p></li><li><a href="index.php?page='.$total.'">'.$total.'</a></li>';
+}else
+{
+$strtotal = "";
+}
+
+if ($total > 1)
+{
+echo '
+<div class="pstrnav">
+<ul>
+';
+echo $pstr_prev.$page5left.$page4left.$page3left.$page2left.$page1left."<li><a class='pstr-active' href='index.php?page=".$page."'>".$page."</a></li>".$page1right.$page2right.$page3right.$page4right.$page5right.$strtotal.$pstr_next;
+echo '
+</ul>
+</div>
+';
+}
+			?>
+				
 		</div>
 <?php 
 	include 'include/block-footer.php';
  ?>
 </div>
-
 </body>
 </html>
--- view_cat.php
+++ view_cat.php
@@ -91,7 +91,22 @@
 						$catlink = "";
 					}
 				}
-				$result = mysql_query("SELECT * FROM table_products WHERE visible = '1' $querycat ORDER BY $sorting",$link);
+					$num = 5;
+	$page = (int)$_GET['page'];
+	$count = mysql_query("SELECT COUNT(*) FROM table_products WHERE visible = '1' $querycat",$link);
+	$temp = mysql_fetch_array($count);
+	If ($temp[0] > 0)
+	{
+	$tempcount = $temp[0];
+	$total = (($tempcount - 1) / $num) + 1;
+	$total =  intval($total);
+	$page = intval($page);
+	if(empty($page) or $page < 0) $page = 1;
+	if($page > $total) $page = $total;
+	$start = $page * $num - $num;
+	$qury_start_num = " LIMIT $start, $num";
+	}
+				$result = mysql_query("SELECT * FROM table_products WHERE visible = '1' $querycat ORDER BY $sorting $qury_start_num",$link);
 				if (mysql_num_rows($result) > 0)
 				{
 					$row = mysql_fetch_array($result);
@@ -155,7 +170,7 @@
 			 	</ul>
 			 	<ul id="block-tovar-list">
 			<?php 
-				$result = mysql_query("SELECT * FROM table_products WHERE visible = '1' $querycat ORDER BY $sorting",$link);
+				$result = mysql_query("SELECT * FROM table_products WHERE visible = '1' $querycat ORDER BY $sorting $qury_start_num",$link);
 				if (mysql_num_rows($result) > 0)
 				{
 					$row = mysql_fetch_array($result);
@@ -200,8 +215,46 @@
 				{
 					echo '<h3>Категория не доступна или не создана!</h3>';
 				}
+						echo '</ul>';
+if ($page != 1){ $pstr_prev = '<li><a class="pstr-prev" href="view_cat.php?page='.($page - 1).'">&lt;</a></li>';}
+if ($page != $total) $pstr_next = '<li><a class="pstr-next" href="view_cat.php?page='.($page + 1).'">&gt;</a></li>';
+
+
+// Формируем ссылки со страницами
+if($page - 5 > 0) $page5left = '<li><a href="view_cat.php?page='.($page - 5).'">'.($page - 5).'</a></li>';
+if($page - 4 > 0) $page4left = '<li><a href="view_cat.php?page='.($page - 4).'">'.($page - 4).'</a></li>';
+if($page - 3 > 0) $page3left = '<li><a href="view_cat.php?page='.($page - 3).'">'.($page - 3).'</a></li>';
+if($page - 2 > 0) $page2left = '<li><a href="view_cat.php?page='.($page - 2).'">'.($page - 2).'</a></li>';
+if($page - 1 > 0) $page1left = '<li><a href="view_cat.php?page='.($page - 1).'">'.($page - 1).'</a></li>';
+
+if($page + 5 <= $total) $page5right = '<li><a href="view_cat.php?page='.($page + 5).'">'.($page + 5).'</a></li>';
+if($page + 4 <= $total) $page4right = '<li><a href="view_cat.php?page='.($page + 4).'">'.($page + 4).'</a></li>';
+if($page + 3 <= $total) $page3right = '<li><a href="view_cat.php?page='.($page + 3).'">'.($page + 3).'</a></li>';
+if($page + 2 <= $total) $page2right = '<li><a href="view_cat.php?page='.($page + 2).'">'.($page + 2).'</a></li>';
+if($page + 1 <= $total) $page1right = '<li><a href="view_cat.php?page='.($page + 1).'">'.($page + 1).'</a></li>';
+
+if ($page+5 < $total)
+{
+$strtotal = '<li><p class="nav-point">...</p></li><li><a href="view_cat.php?page='.$total.'">'.$total.'</a></li>';
+}else
+{
+$strtotal = "";
+}
+
+if ($total > 1)
+{
+echo '
+<div class="pstrnav">
+<ul>
+';
+echo $pstr_prev.$page5left.$page4left.$page3left.$page2left.$page1left."<li><a class='pstr-active' href='view_cat.php?page=".$page."'>".$page."</a></li>".$page1right.$page2right.$page3right.$page4right.$page5right.$strtotal.$pstr_next;
+echo '
+</ul>
+</div>
+';
+}
 			 ?>
-			 	</ul>
+
 		</div>
 <?php 
 	include 'include/block-footer.php';
