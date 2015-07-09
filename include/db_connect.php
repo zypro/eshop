ee7e09278593f89467a712a39eab63b32afa7959
Valijon <parliament888@gmail.com>
2015-07-09 16:33:25 +0500

    changed encoding from cp1251 to utf8

--- include/db_connect.php
+++ include/db_connect.php
@@ -7,5 +7,5 @@ $db_database	= 'parliaub_eshop';
 $link = mysql_connect($db_host,$db_user,$db_pass);
 
 mysql_select_db($db_database,$link) or die("Нет соединения с БД ".mysql_error());
-mysql_query("SET names cp1251");
+mysql_query("SET names utf8");
 ?>
