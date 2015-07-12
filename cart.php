d2eb411cf4bca2010c010f4ab3e82b2d8b9c0621
Valijon <parliament888@gmail.com>
2015-07-12 13:37:23 +0500

    unicode

--- cart.php
+++ cart.php
@@ -96,7 +96,7 @@ $int = $int + ($row["price"] * $row["cart_count"]);
 <!DOCTYPE html">
 
 <head>
-	<meta "charset=utf-8">
+	<meta charset="utf-8">
 	<link href="css/reset.css" rel="stylesheet" type="text/css">
 	<link href="css/style.css" rel="stylesheet" type="text/css">
 	<link href="trackbar/trackbar.css" rel="stylesheet" type="text/css">
