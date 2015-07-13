400d1e87a08cacd7e2c383b3ac694bbebeb5ee55
Valijon <parliament888@gmail.com>
2015-07-13 16:56:25 +0500

    Админ панель - Авторизация.

---
+++ admin/include/functions.php
@@ -0,0 +1,11 @@
+<?php
+defined('myeshop') or die('Доступ запрещён!');
+	function clear_string($cl_str)
+	{
+	// Фун-я удаление строк
+	$cl_str = strip_tags($cl_str); // удалеине html и php тегов
+	$cl_str = mysql_real_escape_string($cl_str); // очистка спец. символов. Не работает без подкл. в БД
+	$cl_str = trim($cl_str); // фун-я удаление пробелов
+	return $cl_str;
+	}
+ ?>
